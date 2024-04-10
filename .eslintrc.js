0da2018e2a2fae6925ce57b098226029163762ae
polovnikovAI <head080203@gmail.com>
2024-04-10 19:02:11 +0500

    eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'no-var': 'error',
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+};
